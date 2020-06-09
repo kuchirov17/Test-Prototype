c7c2108b5bb864323b6be849218b446f499994a7
sergeantIbra <54941210+sergeantChill@users.noreply.github.com>
2020-06-10 01:04:30 +0300

    Initial commit

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+	public bool player = false;
+	public string characterName;
+
+	[Header("Movement")]
+	public float speed = 2f;
+	public float turnSpeed = 14f;
+
+	protected Rigidbody rb;
+	protected Vector3 curDir;
+	protected Quaternion targetRot;
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	public virtual void FixedUpdate()
+	{
+		rb.AddForce(transform.forward * speed, ForceMode.VelocityChange);
+
+		if (curDir != Vector3.zero)
+		{
+			targetRot = Quaternion.LookRotation(curDir);
+			if(rb.rotation != targetRot)
+			{
+				rb.rotation = Quaternion.RotateTowards(rb.rotation, targetRot, turnSpeed);
+			}
+		}
+	}
+	
+	
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager gm;
+	public MainMenu mm;
+
+	private void Awake()
+	{
+		if (!gm)
+		{
+			gm = this;
+			DontDestroyOnLoad(this);
+		}
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{			
+			RestartScene();			
+		}
+	}
+
+	public void RestartScene()
+	{
+		Time.timeScale = 1f;
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+	public void GameOver()
+	{
+		Time.timeScale = 0;
+		mm.GameOver();
+	}
+
+}
